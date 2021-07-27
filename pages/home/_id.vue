efc17c4dc58dd7254eb695030c56b7e436a1b45c
ViktorXs <viktor_m6@web.de>
2021-07-27 15:37:54 +0200

    Added dynamic routing for each object in database "/data/homes.json" for URLs from a to d, e.g. like so: "http://localhost:3000/home/a"

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -1,15 +1,27 @@
 <template>
     <div>
-        <h2>My Home</h2>
+        <img :src="home.images[0]" style="width:200px"><br>
+        <h3>{{ home.title }}</h3>
+        <p>{{ home.location.address }}<br>
+        {{ home.location.city }}<br>
+        {{ home.location.state }}, {{ home.location.country }}</p><br>
+        <p>Guests: {{ home.guests }}</p><br>
+        <p>{{ home.pricePerNight }} $ per night</p>
     </div>
 </template>
 
 <script>
-/* import homes from "~/data/homes" */ 
+import homes from "~/data/homes"
 
 export default {
-    created(){  /* <- Lifecycle Hook */
-        console.log(this.$route)
+    data(){
+        return {
+            homes: {}
+        }
+    },
+    created(){
+        const home = homes.find((home) => home.objectID === this.$route.params.id)
+        this.home = home
     }
 }
 </script>
