76e09aeda705f762dfe4c26cb86ba7414d83f6f5
ViktorXs <viktor_m6@web.de>
2021-07-27 10:38:55 +0200

    Creating Project

---
+++ components/HomeCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div style="margin: 10px">
+        <h3>{{ home.title }}</h3>
+        <img :src="home.images[0]" style="width:200px" />
+        <p>{{ home.type }}</p>
+        <p>{{ home.location.address }}<br>{{ home.location.city }}<br>{{ home.location.state }}, {{ home.location.country }}</p>
+        <!-- <p>{{ home.description }}</p> -->
+        <p>{{ home.pricePerNight }} / night</p>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        home: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div style="margin:20px">
+    <h1>Some nice homes</h1>
+    <div v-for="home in homes" :key="home.objectID" style="float:left">
+      <home-card :home="home" />
+    </div>
+  </div>
+</template>
+
+<script>
+  import homes from "~/data/homes"
+  /* import HomeCard from "~/components/HomeCard" */ 
+
+  export default {
+  /*  components: {
+        HomeCard,
+    }, */
+    data(){
+      return {
+        homes: homes.slice(0,3)
+      }
+    }
+  }
+</script>
