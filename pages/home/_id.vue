4c42141e00e869205f773197690397edb155a16b
ViktorXs <viktor_m6@web.de>
2021-07-27 14:33:50 +0200

    Learning dynamic routing

---
+++ pages/home/_id.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>My Home</h2>
+    </div>
+</template>
+
+<script>
+/* import homes from "~/data/homes" */ 
+
+export default {
+    created(){
+        console.log(this.$route)
+    }
+}
+</script>
