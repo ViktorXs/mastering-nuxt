754426e38a59c97d5e23b780d9567b7022ea3506
ViktorXs <viktor_m6@web.de>
2021-07-28 15:41:54 +0200

    Updated progress. Stopping prefetching as a global configuration in "config.nuxt.js".

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,25 +1,28 @@
 export default {
     components: true,
     head: {
-      titleTempalte: "Mastering Nuxt: %s",
-      htmlAttrs: {
-        lang: "en"
-      },
-      bodyAttrs: {
-        class: ["my-style"]
-      },
-      meta: [{
+        titleTempalte: "Mastering Nuxt: %s",
+        htmlAttrs: {
+            lang: "en"
+        },
+        bodyAttrs: {
+            class: ["my-style"]
+        },
+        meta: [{
           charset: "utf-8"
-      }]
+        }]
     },
-  
+
+    router: {
+        prefetchLinks: false
+    },
+
     // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
     buildModules: [
-      // https://go.nuxtjs.dev/eslint
-      '@nuxtjs/eslint-module',
-      // https://go.nuxtjs.dev/vuetify
-      '@nuxtjs/vuetify',
+        // https://go.nuxtjs.dev/eslint
+        '@nuxtjs/eslint-module',
+        // https://go.nuxtjs.dev/vuetify
+        '@nuxtjs/vuetify',
     ],
-  
-  }
-  
+
+}
