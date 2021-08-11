c95e9ad2b23284e513ee8ad23aba8542a5ecc96d
ViktorXs <viktor_m6@web.de>
2021-08-11 16:05:53 +0200

    Finished lesson "Refactoring for Google Places".
    Refactored code inside "maps.client.js" plugin for being able to load any google API in an array and wait until everything is loaded.

--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -1,7 +1,7 @@
 export default function(context, inject){
 
     let mapLoaded = false
-    let mapWaiting = null
+    let waiting = []
 
     addScript()
     inject("maps", {
@@ -18,20 +18,25 @@ export default function(context, inject){
 
     function initMap(){
         mapLoaded = true
-        if (mapWaiting){
-            const { canvas, lat, lng } = mapWaiting
-            renderMap(canvas, lat, lng)
-            mapWaiting = null
-        }
+
+        waiting.forEach((item) => {  /* gehe durch jede Funktion in variable waiting */
+            if(typeof item.fn === "function") {  /* wenn item als Typ eine Funktion ist... */
+                item.fn(...item.arguments)      /* ... = spread. alle Parameter der Funktion item geben */
+            }
+        })
+        waiting = []  /* Wenn alles geladen ist, kann waiting geleert werden */     
     }
 
     function showMap(canvas, lat, lng){
-        if(mapLoaded) renderMap(canvas, lat, lng)
-        else mapWaiting = { canvas, lat, lng }
-    }
+        if(!mapLoaded) {
+            waiting.push({      /* Folgende zwei Eigenschaften in das Array waiting einfügen */
+                fn: showMap,    /* fn = Funktion = Nicht in Anführungsstrichen. Referenz zur aktuellen Funktion */
+                arguments       /* arguments ist eine Variable von JS. Alle Variablen einer Funktion */
+            })
+            return              /* nichts tun, solange die map nicht geladen ist.  */
+            }  /* alles was hiernach steht, ist Code, der ausgeführt werden kann, wenn google geladen ist  */
 
-    function renderMap(canvas, lat, lng){
-        const mapOptions = {
+        const mapOptions = {  /* ehemals renderMap Funktion */
             zoom: 18,
             center: new window.google.maps.LatLng(lat, lng),
             disableDefaultUI: true,
