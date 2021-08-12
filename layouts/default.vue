3154f11efba3485156ccf0ea384d2545eafa83bc
ViktorXs <viktor_m6@web.de>
2021-08-12 12:10:20 +0200

    Finished lesson "Google Places Autocomplete"
    - Added Event Listener to be able autofill searched places with Google Places Autocomplete and returning a page with the search result.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,34 @@
 <div>
     <header style="background-color:#ddd">
         <nuxt-link to="/">Home</nuxt-link>
+        <input ref="citySearch" type="text" @changed="changed" />  <!-- "@changed" = Custom Event aus maps.client.js. "=changed"-Funktion -->
     </header>
     <nuxt />
 </div>
 </template>
+
+<script>
+export default {
+    mounted() {
+        this.$maps.doAutoComplete(this.$refs.citySearch);  /* der input für doAutoComplete(input) */
+    },
+    
+    methods: {
+        changed(event) {
+            const place = event.detail
+            if(!place.geometry)
+                return  /* "If it doesnt exist, just return" */
+
+            /* User auf eine neue Seite leiten, dabei label und die Längen- und Breitengrade durchgeben. */
+            this.$router.push({  /* push nimmt String oder Objekt. Damit die URL generieren */
+                name: "search",
+                query: {
+                    lat: place.geometry.location.lat(),
+                    lng: place.geometry.location.lng(),
+                    label: this.$refs.citySearch.value  /* der URL ein label geben */
+                } 
+            })
+        }
+    }
+}
+</script>
--- plugins/maps.client.js
+++ plugins/maps.client.js
@@ -5,7 +5,8 @@ export default function(context, inject){
 
     addScript()
     inject("maps", {
-        showMap
+        showMap,
+        doAutoComplete
     })
 
     function addScript(){
@@ -16,27 +17,45 @@ export default function(context, inject){
         document.head.appendChild(script)
     }
 
-    function initGoogleMaps(){
+    function initGoogleMaps() {
         isLoaded = true
 
-        waiting.forEach((item) => {  /* gehe durch jede Funktion in variable waiting */
-            if(typeof item.fn === "function") {  /* wenn item als Typ eine Funktion ist... */
-                item.fn(...item.arguments)      /* ... = spread. alle Parameter der Funktion item geben */
+        waiting.forEach((item) => {
+            if(typeof item.fn === "function") {
+                item.fn(...item.arguments)
             }
         })
-        waiting = []  /* Wenn alles geladen ist, kann waiting geleert werden */     
+        waiting = []
     }
 
-    function showMap(canvas, lat, lng){
-        if(!isLoaded) {
-            waiting.push({      /* Folgende zwei Eigenschaften in das Array waiting einfügen */
+    function doAutoComplete(input) {    /* Google Places Autocomplete */
+        if(!isLoaded) {                 /* Wenn nicht geladen... */
+            waiting.push({ fn: doAutoComplete, arguments }) /* ... in das waiting Array diese Funktion mit ihren Paramemtern einfügen */
+            return  /* Wenn nichts geladen, nichts ausführen */
+        }
+        /* Ausführen, wenn geladen */
+        /* Autocomplete über addScript() verfügbar dank &libraries=places */
+        
+        const options = {
+            types: ["(cities)"]
+        }
+        const autoComplete = new window.google.maps.places.Autocomplete(input, options)  /* types ist ein Array aus String-Werten von erlaubten Einheiten */
+        autoComplete.addListener("place_changed", () => {  /* 1. argument: "name of the event to listen to." 2. argument: "callback function" */
+            const place = autoComplete.getPlace()  /* Was per autoComplete gefunden/ausgewählt wurde, holt sich getPlace() die Informationen des Ortes. */
+            input.dispatchEvent(new CustomEvent("changed", { detail: place }))  /* Emit an Event of our own directly on the input element */
+        })
+    }
+
+    function showMap(canvas, lat, lng) {
+        if(!isLoaded) {         /* Wenn nicht geladen = false ... */
+            waiting.push({      /* ... folgende zwei Eigenschaften in das Array waiting einfügen */
                 fn: showMap,    /* fn = Funktion = Nicht in Anführungsstrichen. Referenz zur aktuellen Funktion */
                 arguments       /* arguments ist eine Variable von JS. Alle Variablen einer Funktion */
             })
-            return              /* nichts tun, solange die map nicht geladen ist.  */
-            }  /* alles was hiernach steht, ist Code, der ausgeführt werden kann, wenn google geladen ist  */
+            return              /* nichts ausführen, solange die map nicht geladen ist.  */
+        }  /* alles was hiernach steht, ist Code, der ausgeführt werden kann, wenn google geladen ist  */
 
-        const mapOptions = {  /* ehemals renderMap Funktion */
+        const mapOptions = {
             zoom: 18,
             center: new window.google.maps.LatLng(lat, lng),
             disableDefaultUI: true,
