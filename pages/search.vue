e5a1d671a84262e9a0d8bb9b387a8bc4d1cab5da
ViktorXs <viktor_m6@web.de>
2021-08-13 12:14:05 +0200

    Fixed small error, which the linked homes from the search result page didn't opened the home pages correctly.

--- pages/search.vue
+++ pages/search.vue
@@ -2,7 +2,7 @@
 <div>
     <div><p>Place: {{ lat }} / {{ lng }} / {{ label }}</p></div>
     <div v-for="home in homes" :key="home.ObjectID">
-        <nuxt-link :to="`~/home/${home.objectID}`" prefetch><home-card :home="home" /></nuxt-link>  <!-- Suchergebnisse per nuxt-link verknüpfen, wie auf der index page -->
+        <nuxt-link :to="`/home/${home.objectID}`" prefetch><home-card :home="home" /></nuxt-link>  <!-- Suchergebnisse per nuxt-link verknüpfen, wie auf der index page -->
     </div>
 </div>
 </template>
