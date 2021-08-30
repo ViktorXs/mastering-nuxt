fadf27e249e52e0d732e4997814ac3b9ad7befce
ViktorXs <viktor_m6@web.de>
2021-08-30 13:20:15 +0200

    Finished lesson "Adding a Map to the Search Results Page".
    - Almost copy & pasting code from the id page for importing the google maps view and feeding it with the local data from the search result
    - Map should update, when new search is initialized.
    - Updated the first text about latitude and longitude on the search result page.

--- pages/search.vue
+++ pages/search.vue
@@ -1,10 +1,11 @@
 <template>
 <div>
-    <div><p>Place: {{ lat }} / {{ lng }} / {{ label }}</p></div>
-    <div v-if="homes.length > 0">  <!-- Wenn keine Objekte gefunden werden... -->
+    <div><p>Results for: {{ label }}</p></div>  <!-- Ergebnisse für gesuchten Ort -->
+    <div ref="map" style="width: 800px; height:800px; float: right"></div>  <!-- Map einfügen -->
+    <div v-if="homes.length > 0">
         <HomeRow v-for="home in homes" :key="home.ObjectID" :home="home" />
     </div>
-    <div v-else>No results found.</div>  <!-- ... dann "No results found" -->
+    <div v-else>No results found.</div>
 </div>
 </template>
 
@@ -16,6 +17,7 @@ export default {
         this.label = to.query.label
         this.lat = to.query.lat
         this.lng = to.query.lng
+        this.updateMap()  /* Map soll ebenso geladen werden, wenn neue URL auf die gleiche page führt */
         next()
     },
 
@@ -30,9 +32,19 @@ export default {
         }
     },
 
-    head(){
+    head() {
         return {
-            title: `Homes around ${this.label}`  /* Name des Suchergebnisses im Titel angeben */
+            title: `Homes around ${this.label}`
+        }
+    },
+
+    mounted() {
+        this.updateMap()  /* Map aktualisieren, wenn die Page geladen ist */
+    },
+
+    methods: {
+        updateMap(){
+            this.$maps.showMap(this.$refs.map, this.lat, this.lng) /* Map laden */
         }
     }
 }
