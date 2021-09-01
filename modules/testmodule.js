437498834b602e2408f71d2b0aba873c630d7e4c
ViktorXs <viktor_m6@web.de>
2021-09-01 14:11:48 +0200

    Finished lesson "Module Hooks"
    - Tested some hooks in the testmodule.

--- modules/testmodule.js
+++ modules/testmodule.js
@@ -1,3 +1,8 @@
 export default function() {
-
+    this.nuxt.hook("build:before", () => {
+        console.log("Built before!")
+    })
+    this.nuxt.hook("render:route", (url, result, context) => {  /* Wird an jedem Serverside Route hit ausgeführt */
+        context.res.setHeader("X-Powered-By", "The Force")  /* 2 Argumente: Key & Wert */
+    })
 }
