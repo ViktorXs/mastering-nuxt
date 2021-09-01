28f427efe5bdea6482678b42d4ef7a33c901b7e7
ViktorXs <viktor_m6@web.de>
2021-09-01 14:52:14 +0200

    Made small changes in testmodule.js.

--- modules/testmodule.js
+++ modules/testmodule.js
@@ -1,8 +1,11 @@
 export default function() {
+    this.nuxt.hook("ready", () => {
+        console.log("Nuxt is ready")
+    })
     this.nuxt.hook("build:before", () => {
         console.log("Built before!")
     })
     this.nuxt.hook("render:route", (url, result, context) => {  /* Wird an jedem Serverside Route hit ausgeführt */
-        context.res.setHeader("X-Powered-By", "The Force")  /* 2 Argumente: Key & Wert */
+        context.res.setHeader("X-Powered-By", "The Force")  /* 2 Argumente für Header: Key & Wert */
     })
 }
