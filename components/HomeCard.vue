ab9d7a588e4eecff0ab615fbd1ad8940faf61161
ViktorXs <viktor_m6@web.de>
2021-09-02 12:05:56 +0200

    Added missing function.
    - Added function on HomeCard component to transform singular words into plurals, when they should be.

--- components/HomeCard.vue
+++ components/HomeCard.vue
@@ -2,11 +2,11 @@
 <div>
     <img :src="home.images[0]" style="width:200px"><br>
     <h3>{{ home.title }}</h3>
-    <p>{{ home.location.address }}<br>
+    {{ home.location.address }}<br>
     {{ home.location.city }}<br>
-    {{ home.location.state }}, {{ home.location.country }}</p><br>
-    <p>Guests: {{ home.guests }}</p><br>
-    <p>{{ home.pricePerNight }} $ per night</p>
+    {{ home.location.state }}, {{ home.location.country }}<br>
+    {{ toPlural(home.guests, "guest") }}<br>
+    {{ home.pricePerNight }} $ per night
 </div>
 </template>
 
@@ -16,6 +16,16 @@ export default {
         home: {
             type: Object,
             required: true
+        }
+    },
+
+    methods: {
+        toPlural(number, singularWord) {
+            const text = `${number} ${singularWord}`
+            if(number === 1)
+                return text
+            
+            return text + "s"
         }
     }
 }
