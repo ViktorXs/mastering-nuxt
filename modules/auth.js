d83734f1e01cb71831df6543a00a7777bda4abc0
ViktorXs <viktor_m6@web.de>
2021-09-14 10:51:46 +0200

    Finished lesson "Building an API Firewall 1/2"
    - Wrote idToken constant for parsing a cookie to save the name from the headers of the cookie

--- modules/auth.js
+++ modules/auth.js
@@ -1,14 +1,20 @@
 import cookie from "cookie"
 
 export default function() {  /* Module müssen als Funktionen exportiert werden */
-    const authConfig = this.options.publicRuntimeConfig.auth /* this.options. erlaubt aus dem modul container die nuxt config zu erreichen und zu speichern */
+    const authConfig = this.options.publicRuntimeConfig.auth /* this.options. erlaubt aus dem modul container die nuxt config zu erreichen */
 
     this.nuxt.hook("render:setupMiddleware", (app) => {
         app.use("/api", handler)  /* Nimmt eine Funktion (die handler Funktion unten), welche auf eine gewisse Route reagiert "/api" */
     })    
 
-    function handler(req, res, next) {  /* Middleware */
+    function handler(req, res, next) {  /* Middleware handler */
+        const idToken = cookie.parse(req.headers.cookie) [authConfig.cookieName]
+        
+        /* Zur Kontrolle */
         console.log(req.url)
+        console.log(idToken)
+
+        /* next() notwendig, damit nächste Middleware lädt */
         next()
     }
 }
